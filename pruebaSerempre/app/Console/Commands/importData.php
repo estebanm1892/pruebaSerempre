777ebb4667f93bd4eb812487beabc147b4185fcc
Esteban Monroy B <estebanm1892@gmail.com>
2020-09-03 18:25:08 -0500

    end import-data:db command funtions

--- pruebaSerempre/app/Console/Commands/importData.php
+++ pruebaSerempre/app/Console/Commands/importData.php
@@ -4,6 +4,14 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use Illuminate\Database\Eloquent\Model;
+
+use App\City;
+
+use App\Client;
+
+use App\User;
+
 class importData extends Command
 {
     /**
@@ -37,8 +45,74 @@ class importData extends Command
      */
     public function handle()
     {
-        $this->call('migrate');
-        $this->call('serve');
+        // Ejecución de migraciones
+        $this->call('migrate');        
+
+        $this->info('Migraciones creadas');
+        
+        // Inserción de datos a las migraciones ciudades
+        
+        $this->info('Insertando datos de ciudades');
+
+        $this->output->progressStart(5);
+
+        $data1 = array(
+            array('name'=>'Tunja'),
+            array('name'=>'Sogamoso'),
+            array('name'=>'Bogota'),
+            array('name'=>'Medellin'),
+            array('name'=>'Cali')
+            
+        );
+        
+        $this->output->progressAdvance();
+
+        City::insert($data1); // Eloquent approach       
+
+        $this->output->progressFinish();
+
+        $this->info('Datos de ciudades insertados');
+
+        // Inserción de datos a las migraciones clientes
+        
+        $this->info('Insertando datos de clientes');
+
+        $this->output->progressStart(3);
+
+        $data2 = array(
+            array('name'=>'John Snow', 'city_id' => 2),
+            array('name'=>'Draenerys Targaryen', 'city_id' => 3),
+            array('name'=>'Tyrion Lannister', 'city_id' => 4),            
+        );
+        
+        $this->output->progressAdvance();
+
+        Client::insert($data2); // Eloquent approach       
+
+        $this->output->progressFinish();
+
+        $this->info('Datos de clientes insertados');
+
+        // Inserción de datos a las migraciones usuarios
+        
+        $this->info('Insertando datos de usuarios');
+
+        $this->output->progressStart(3);
+
+        $data3 = array(
+            array('name'=>'Esteban Monroy', 'email' => 'estebanm1892@gmail.com', 'password' => bcrypt(12345)),
+            array('name'=>'Yenkary Peralta', 'email' => 'luyen@gmail.com', 'password' => bcrypt(12345)),
+            array('name'=>'Serrato', 'email' => 'serrato@gmail.com', 'password' => bcrypt(12345)),
+        );
+        
+        $this->output->progressAdvance();
+
+        User::insert($data3); // Eloquent approach       
+
+        $this->output->progressFinish();
+
+        $this->info('Datos de usuarios insertados');
+
 
     }
 }
